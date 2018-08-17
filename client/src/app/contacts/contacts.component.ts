a3ca7329620f249ddde883c47c4f3bac25fa62b5
ashutosh <amxdp18@gmail.com>
2018-08-18 02:40:12 +0530

    adding contacts

--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -15,8 +15,26 @@ export class ContactsComponent implements OnInit {
   first_name: string;
   last_name: string;
   phone: string;
-
+  msg: string;
   constructor(private contactService: ContactService) { }
+
+ addContact() {
+   const newContact = {
+     first_name: this.first_name,
+     last_name: this.last_name,
+     phone: this.phone,
+     msg: this.msg
+   };
+   this.contactService.addContact(newContact)
+   .subscribe(contact => {
+     this.contacts.push(contact);
+     this.contactService.getContacts()
+     .subscribe(contacts => this.contacts = contacts);
+     this.msg = 'Added successfully';
+   });
+
+ }
+
 
   deleteContact(id: any) {
     const contacts = this.contacts;
