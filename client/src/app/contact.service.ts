13e1e099006f4f9d98a61ade11128358fced054e
ashutosh <amxdp18@gmail.com>
2018-08-17 16:28:18 +0530

    npm update and conatct.ts added

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import {Contact} from './contact';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private http: Http) {   }
+// retreving contact
+
+getContacts() {
+  return this.http.get('htttp://localhost:3000/api/contacts')
+  .pipe(map(res => res.json()));
+}
+ addContact(newContact) {
+   const headers = new Headers();
+   headers.append('Content-Type', 'application/json');
+   return this.http.post('http://localhost:3000/api/contact', newContact, {headers: headers})
+   .pipe(map(res => res.json()));
+
+ }
+
+ deleteContact(id) {
+   return this.http.delete('http://localhost:3000/api/contact/' + id)
+   .pipe(map(res => res.json()));
+ }
 }
